ed6c30e9e34ffa4da03bdab1b78e486c17812431
tanishabisht <agamyatani@gmail.com>
2022-02-20 00:28:58 +0530

    line z-index fix

--- linerender.js
+++ linerender.js
@@ -8,17 +8,32 @@ var addBtn = document.querySelector("#addBtn");
 var grid = document.querySelectorAll(".grid");
 
 function onAddLineInSvg() {
-  console.log("setting up line");
+  var node_ = $(".node");
+  var switch_ = $(".switch");
+  var grid_ = $(".grid");
+
   $(
     `<line x1="${y1 + 10}" y1="${x1 + 10}" x2="${y2 + 10}" y2="${
       x2 + 10
     }" style='stroke: white; stroke-width: 3px'></line>`
   ).prependTo(document.querySelector("svg"));
   $(".lines").html($(".lines").html());
+
+  node_.css({ "z-index": "1" });
+  switch_.css({ "z-index": "1" });
+  grid_.css({ "z-index": "-1" });
 }
 setBtn.click(onAddLineInSvg);
 
 addBtn.addEventListener("click", () => {
+  var node_ = $(".node");
+  var switch_ = $(".switch");
+  var grid_ = $(".grid");
+
+  node_.css({ "z-index": "-1" });
+  switch_.css({ "z-index": "-1" });
+  grid_.css({ "z-index": "1" });
+
   grid.forEach((item) => {
     item.addEventListener("click", () => {
       var x = gsap.getProperty(item, "top");
